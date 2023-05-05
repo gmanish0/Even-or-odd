4f377b3b835c082fa3a2d41e1efe1c7642df7c48
Manish Gupta <131979726+gmanish0@users.noreply.github.com>
2023-05-05 17:23:14 +0530

    Add files via upload

---
+++ evenodd.java
@@ -0,0 +1,16 @@
+package manish;
+
+public class evenodd {
+
+	public static void main(String[] args) {
+	int n=37;
+	if(n%2==0)
+	{
+		System.out.println("the given number is even");
+	}
+	else
+		System.out.println("the given number is odd");
+
+	}
+
+}
